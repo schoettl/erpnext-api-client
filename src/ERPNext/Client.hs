1fe7901209c27e51c694a8ec58fd7df376196ac3
Jakob Schöttl <jschoett@gmail.com>
2025-03-12 16:30:11 +0100

    Updates and fixes

--- src/ERPNext/Client.hs
+++ src/ERPNext/Client.hs
@@ -1,53 +1,70 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE TypeApplications #-}
+
 module ERPNext.Client
   ( getDocTypeList
   , getDocType
   , postDocType
   , putDocType
   , deleteDocType
-  , mkConfig
   , mkSecret
+  , mkConfig
+  , withTlsSettings
+  , IsDocType (..)
   , Config ()
   , Secret ()
+  , ApiResponse (..)
   ) where
 
 import Network.HTTP.Client
 import Data.Text
+import Data.Aeson
 
-getDocTypeList :: Config -> IO [a]
-getDocTypeList _ = error "implement"
+-- | Type class for types which represent an ERPNext DocType.
+-- Each DocType has a unique name.
+class IsDocType a where
+  docTypeName :: Text
 
-getDocType :: Config -> Text -> IO (Result a)
+getDocTypeList :: forall a. (IsDocType a, FromJSON a) => Config -> ApiResponse [a]
+getDocTypeList = error (unpack $ docTypeName @a)
+
+getDocType :: Config -> Text -> IO (ApiResponse a)
 getDocType _ _ = error "implement"
 
-deleteDocType :: Config -> Text -> IO (Result a) -- ??
+deleteDocType :: Config -> Text -> IO (ApiResponse a) -- ??
 deleteDocType _ _ = error "implement"
 
-postDocType :: Config -> Text -> a -> IO (Result a)
+postDocType :: Config -> Text -> a -> IO (ApiResponse a)
 postDocType _ _ _ = error "implement"
 
-putDocType :: Config -> Text -> a -> IO (Result a)
+putDocType :: Config -> Text -> a -> IO (ApiResponse a)
 putDocType _ _ _ = error "implement"
 
 mkConfig :: Text -> Text -> Secret -> Config
-mkConfig baseUrl apiUser apiSecret = Config
+mkConfig baseUrl apiKey apiSecret = Config
   { baseUrl = baseUrl
-  , apiUser = apiUser
+  , apiKey = apiKey
   , apiSecret = apiSecret
   , tlsSettings = defaultManagerSettings
   }
 
+withTlsSettings :: Config -> ManagerSettings -> Config
+withTlsSettings c x = c { tlsSettings = x }
+
 mkSecret :: Text -> Secret
 mkSecret = Secret
 
+-- | API client configuration.
 data Config = Config
   { baseUrl :: Text
-  , apiUser :: Text
+  , apiKey :: Text
   , apiSecret :: Secret
   , tlsSettings :: ManagerSettings
   }
 
+-- | Opaque type to store the API secret.
 data Secret = Secret
   { getSecret :: Text
   }
 
-data Result a = Ok a | Error Text
+data ApiResponse a = Ok a | Error Text
